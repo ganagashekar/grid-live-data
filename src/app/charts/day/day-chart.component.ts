c389bfae58b5270bf401950e6431b78afc7b4891
Haadv <Haadv@HAADVISRI>
2023-07-29 23:55:32 +0530

    final

--- src/app/charts/day/day-chart.component.ts
+++ src/app/charts/day/day-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SeriesLine } from '@progress/kendo-angular-charts';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -9,6 +10,7 @@ export class DayChartComponent {
   // @Input() public chartdata : Observable<number[]> | undefined;
     @Input() public data: number[] = [];
     @Input() public changePct: number = 0;
+    public lineStyle: SeriesLine = { width: 2, style: 'smooth', color: '#4B5FFA' };
 
     // constructor() {
     //   setInterval(() => {
@@ -29,21 +31,7 @@ export class DayChartComponent {
     // }
 
     constructor() {
-      setInterval(() => {
-        // Clone the array
-        // const data= this.data.slice(0);
 
-        // // Produce one random value each 100ms
-        // data.push(Math.random()+1);
-
-        // // if (data.length > 10) {
-        // //   // Keep only 10 items in the array
-        // //   data.shift();
-        // // }
-
-        // // Replace with the new array instance
-        // this.data = data;
-      }, 100);
     }
 
     ngOnChanges(changes: any) {
@@ -54,10 +42,10 @@ export class DayChartComponent {
       // Produce one random value each 100ms
       data.push(Math.random()+1);
 
-      // if (data.length > 10) {
-      //   // Keep only 10 items in the array
-      //   data.shift();
-      // }
+      if (data.length > 1000) {
+        // Keep only 10 items in the array
+        data.shift();
+      }
 
       // Replace with the new array instance
       this.data = data;
