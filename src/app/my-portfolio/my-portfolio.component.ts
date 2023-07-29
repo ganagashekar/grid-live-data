833c79ed066c5aa61b7691bf13b1bb764e30d0b5
Haadv <Haadv@HAADVISRI>
2023-07-29 13:39:25 +0530

    1st

---
+++ src/app/my-portfolio/my-portfolio.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { IntlService } from '@progress/kendo-angular-intl';
+import { Observable } from 'rxjs';
+import { Stock } from '../services/stocks.service';
+import { cards } from './cards-data';
+
+@Component({
+    selector: 'my-portfolio',
+    templateUrl: './my-portfolio.component.html',
+    styleUrls: ['./my-portfolio.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class MyPortfolioComponent {
+    @Input() data!: Observable<Stock[]>;
+
+    public cryptoCards: Stock[] = cards;
+
+    constructor(public intl: IntlService) {}
+
+    public getImg(card: any): string {
+        return `assets/coinslogo/${card.currency}.png`;
+    }
+
+    public getPriceChange(card: Stock): number {
+        const  priceChange = (card.currentPrice * card.change_24h/100)/(1 + card.change_24h/100);
+        return priceChange;
+    }
+}
